fa2ddb9ec7b052b9816a2d6bf61a85c19b7127f4
Yevhenii Kolomiiets <aloha.ki990@gmail.com>
2020-06-11 08:57:52 +0300

    Add project files.

---
+++ RestAPITest/Models/UserPostRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPITest.Models
+{
+    class UserPostRequestDto
+    {
+        public string Email { get; internal set; }
+        public string Name { get; set; }
+        public string UserName { get; internal set; }
+    }
+}
---
+++ RestAPITest/Tests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System.Collections.Generic;
+
+
+namespace RestAPITest
+{
+    [TestFixture]
+    public class Tests
+    {
+        private RestClient client;
+        private RestRequest request;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new RestClient("http://jsonplaceholder.typicode.com");
+            request = new RestRequest("users");
+        }
+
+        [Test]
+        public void TestGet_WhenSendUsers_ShouldReturnOk()
+        {
+            request.Method = Method.GET;
+            var response = client.Execute<List<Users>>(request);
+            Assert.AreEqual(true, response.IsSuccessful);
+            Assert.AreNotEqual(0, response.Data.Count);
+
+        }
+    }
+}
